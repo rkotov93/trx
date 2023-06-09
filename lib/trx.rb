61184ba8b79f1da2c3affa78e36c42a63d1c08b0
Ruslan Kotov <rkotov93@gmail.com>
2023-06-09 20:07:34 +0300

    initial commit

---
+++ lib/trx.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative 'trx/version'
+
+module Trx
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/trx/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module Trx
+  VERSION = '0.1.0'
+end
